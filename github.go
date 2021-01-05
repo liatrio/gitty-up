ad6fbb276a6066a065ae8478c567bcc30fd5b10a
Chris Schreiber <chriss@liatrio.com>
2021-01-05 12:20:18 -0800

    add argument to for base git branch (#10)

--- github.go
+++ github.go
@@ -9,14 +9,14 @@ import (
 	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
-func githubPullRequest(httpClient *http.Client, org string, repo string, branch *plumbing.Reference) (pullRequest *github.PullRequest, err error) {
+func githubPullRequest(httpClient *http.Client, org string, repo string, base string, branch *plumbing.Reference) (pullRequest *github.PullRequest, err error) {
 	fmt.Printf("Create pull request for branch %s\n", branch.Name().Short())
 	client := github.NewClient(httpClient)
 
 	newPR := &github.NewPullRequest{
 		Title:               github.String("GitOps"),
 		Head:                github.String(branch.Name().Short()),
-		Base:                github.String("master"),
+		Base:                github.String(base),
 		Body:                github.String(createGitMessage("pull request")),
 		MaintainerCanModify: github.Bool(true),
 	}
